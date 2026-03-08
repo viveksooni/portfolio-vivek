92878c4edeb126a1325e37765d0c55350da71707
Vivek Soni <vivek.soni@iitkfirst.com>
2026-03-08 16:16:29 +0530

    add git deploy file

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ import Navbar from '@/components/Navbar';
 import CustomCursor from '@/components/CustomCursor';
 import Preloader from '../components/Preloader';
 import StickyEmail from './_components/StickyEmail';
-import Script from 'next/script';
 
 const antonFont = Anton({
     weight: '400',
